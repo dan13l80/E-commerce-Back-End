27259a37ef00cbbd9316853fd2bab3a6aa7fab44
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2022-01-21 00:40:23 -0500

    Product models rectified

--- models/index.js
+++ models/index.js
@@ -14,22 +14,6 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
-
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
-
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-})
-
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
@@ -42,14 +26,6 @@ Tag.belongsToMany(Product, {
   foreignKey: 'tag_id'
 
 })
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
-
 
 module.exports = {
   Product,
