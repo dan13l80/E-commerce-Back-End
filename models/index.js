4aa070105f3019b5e112cc7e15085179606be724
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2022-01-21 00:35:18 -0500

    Added models attributes

--- models/index.js
+++ models/index.js
@@ -7,7 +7,23 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-}
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
 Category.hasMany(Product, {
@@ -26,6 +42,14 @@ Tag.belongsToMany(Product, {
   foreignKey: 'tag_id'
 
 })
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
+
 
 module.exports = {
   Product,
